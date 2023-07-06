8ca4e3e7c957591b012a3cabde530646c5fefa1d
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-07-06 13:46:34 -0500

    Agregar archivos de proyecto.

---
+++ Programa04_01/CPersona.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa04_01
+{
+    class CPersona
+    {
+        private string nombre;
+        private int edad;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public int Edad
+        {
+            get { return edad; }
+            set 
+            { 
+                if (value >=18) 
+                    edad = value;
+                else 
+                {  
+                    edad = -1;
+                    Console.WriteLine("Edad no permitida");
+                }
+            }
+        }
+        public void Muestra()
+        {
+            Console.WriteLine("{0} tiene {1} años", nombre, edad);
+        }
+    }
+}
---
+++ Programa04_01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa04_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creamos un objeto de la clase CPersona
+            CPersona uno = new CPersona();
+            uno.Nombre = "Maria";
+            uno.Edad = 27;
+
+            uno.Muestra();
+            Console.WriteLine("-----");
+
+            // Creamos un objeto de la clase CEmpleado
+            CEmpleado dos = new CEmpleado();
+
+            dos.Nombre = "Juan";
+            dos.Edad = 30; 
+            dos.Puesto = "Contador";
+            dos.Salario = 10500.50;
+            dos.EmpleadoMuestra();
+            dos.Muestra();
+            Console.WriteLine("-----");
+            CEmpleado tres = new CEmpleado();
+            tres.ponerDatos("Susana", 25, "Programadora", 150000.30);
+            tres.EmpleadoMuestra();
+
+
+        }
+    }
+}
