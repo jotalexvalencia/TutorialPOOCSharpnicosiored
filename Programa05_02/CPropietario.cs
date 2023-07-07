05385e5e11eeba263dc380e35522c6de3b528127
Alex Valencia <jorgealexandervalencia@gmail.com>
2023-07-07 16:14:40 -0500

    Programa05_02: Composición

---
+++ Programa05_02/CPropietario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa05_02
+{
+    class CPropietario
+    {
+        private string nombre;
+        private CEdificio propiedad = null;
+
+        public CPropietario(string pNombre)
+        {
+            nombre = pNombre;
+            propiedad = new CEdificio("Av 2 número 32",7,4);
+        }
+
+        public void MostrarPropiedad()
+        {
+            if (propiedad != null)
+            {
+                Console.WriteLine("{0} tiene", nombre);
+                propiedad.Muestra();
+            }
+            else
+                Console.WriteLine("{0} aún no tiene propiedades", nombre);
+        }
+
+        //// Cuidado con esro, rome el encapsulamiento
+        //public CEdificio Propiedad
+        //{
+        //    get { return propiedad; }
+        //    set { propiedad = value; }
+        //}
+    }
+}
---
+++ Programa05_02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa05_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // cramos el propietario
+            CPropietario Ana = new CPropietario("Ana");
+
+            // Mostramos la propiedad
+            Ana.MostrarPropiedad();
+            Console.WriteLine("-----");
+
+            // Esto Rompe con la encapsulación
+            // Tratamos de acceder a la propiedad
+            //CEdificio edif = Ana.Propiedad;
+
+            //edif.Muestra();
+            Console.WriteLine("-----");
+
+            // Eliminamos el objeto CPropietario
+            Ana = null;
+            GC.Collect();
+
+            //edif.Muestra();
+        }
+    }
+}
